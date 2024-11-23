ef57f75530f20a9bb84c1c74a521efe4bfa2f445
nickmantini <nman1github@gmail.com>
2024-11-23 16:25:02 -0600

    so man things i dont even know

---
+++ gambit/views/games/minesView.swift
@@ -0,0 +1,14 @@
+//
+//  crashView.swift
+//  gambit
+//
+//  Created by Nick Mantini on 11/20/24.
+//
+
+import SwiftUI
+
+struct plinkoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
---
+++ gambit/views/games/plinko/plinkoView.swift
@@ -0,0 +1,23 @@
+//
+//  crashView.swift
+//  gambit
+//
+//  Created by Nick Mantini on 11/20/24.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct plinkoView: View {
+
+
+    var body: some View {
+        
+    }
+}
+
+struct MultiplierSlot {
+    let multiplier: Double
+    let color: Color
+}
